139bf0515711e413ff61360c3c61b617925d1d55
katarinakoke <katarinakraljevic0@gmail.com>
2024-04-21 10:34:11 +0200

    testing

--- shortcuts-chest-xray/predictions/fairness.py
+++ shortcuts-chest-xray/predictions/fairness.py
@@ -6,12 +6,11 @@ import logging
 import json
 import argparse
 from easydict import EasyDict as edict
+from fairlearn.metrics import equalized_odds_difference, equalized_odds_ratio
 
 parser = argparse.ArgumentParser(description='Train model')
 parser.add_argument('cfg_path', default=None, metavar='CFG_PATH', type=str,
                     help="Path to the config file in yaml format")
-parser.add_argument('save_path', default=None, metavar='SAVE_PATH', type=str,
-                    help="Path to the saved models")
 parser.add_argument('--num_workers', default=8, type=int, help="Number of "
                     "workers for each data loader")
 parser.add_argument('--device_ids', default='0,1,2,3', type=str,
@@ -20,61 +19,7 @@ parser.add_argument('--pre_train', default=None, type=str, help="If get"
                     "parameters from pretrained model")
 parser.add_argument('--resume', default=0, type=int, help="If resume from "
                     "previous run")
-parser.add_argument('--logtofile', default=False, type=bool, help="Save log "
-                    "in save_path/log.txt if set True")
 parser.add_argument('--verbose', default=False, type=bool, help="Detail info")
-
-
-def fairness_metrics(y_pred, y_true, sens_attr):
-    eps = 1e-5
-    groups = np.unique(sens_attr).tolist()
-   
-    max_error = 0
-    min_error = 1
-
-    max_mean_y = 0
-    min_mean_y = 1
-
-    max_mean_y0 = 0  # conditioned on y = 0
-    min_mean_y0 = 1
-   
-    max_mean_y1 = 0
-    min_mean_y1 = 1
-
-    for group in groups:
-        yt = y_true[sens_attr == group].astype('int32')
-        ypt = (y_pred[sens_attr == group]).astype('int32')
-        err = -np.mean(yt * np.log(ypt+eps) + (1-yt)*np.log(1-ypt+eps))
-        mean_y = np.mean(y_pred[sens_attr == group])
-        neg = np.logical_and(sens_attr == group, y_true == 0)
-        pos = np.logical_and(sens_attr == group, y_true == 1)
-        mean_y0 = np.mean(y_pred[neg])
-        mean_y1 = np.mean(y_pred[pos])
-      
-        if err > max_error:
-            max_error = err
-        if err < min_error:
-            min_error = err
-
-        if mean_y > max_mean_y:
-            max_mean_y = mean_y
-        if mean_y < min_mean_y:
-            min_mean_y = mean_y
-
-        if mean_y0 > max_mean_y0:
-            max_mean_y0 = mean_y0
-        if mean_y0 < min_mean_y0:
-            min_mean_y0 = mean_y0
-
-        if mean_y1 > max_mean_y1:
-            max_mean_y1 = mean_y1
-        if mean_y1 < min_mean_y1:
-            min_mean_y1 = mean_y1
-    
-    eo = 0.5*(max_mean_y0 - min_mean_y0 + max_mean_y1 - min_mean_y1)
-    dp = max_mean_y - min_mean_y
-    err_parity = max_error - min_error
-    return eo, dp, err_parity
 
 def run(args):
     with open(args.cfg_path) as f:
@@ -82,24 +27,17 @@ def run(args):
         if args.verbose is True:
             print(json.dumps(cfg, indent=4))
 
-    if not os.path.exists(args.save_path):
-        os.mkdir(args.save_path)
-    if args.logtofile is True:
-        logging.basicConfig(filename=args.save_path + '/log.txt',
-                            filemode="w", level=logging.INFO)
-    else:
-        logging.basicConfig(level=logging.INFO)
-
-    if not args.resume:
-        with open(os.path.join(args.save_path, 'cfg.json'), 'w') as f:
-            json.dump(cfg, f, indent=1)
-
-    predictions = pd.read_csv(f'{cfg.pred_model, ".csv"}', index_col=0)
+    predictions = pd.read_csv(cfg.pred_csv)
     
     y_pred = predictions['y_pred_Problem_in']
-    y_true = predictions['Pneumothorax']
 
-    fairness_metrics(y_pred, y_true, cfg.sensitive_attribute)
+    y_true = predictions['Pneumothorax'].map({1.: 1 , 0.: 0, -1.: 0}).fillna(0)
+    sens_features = predictions[cfg.sensitive_attribute]
+    # print(sens_features)
+
+    print(equalized_odds_difference(y_true, y_pred, sensitive_features=sens_features))
+
+    print(equalized_odds_ratio(y_true, y_pred, sensitive_features=sens_features))
 
 def main():
     args = parser.parse_args()
--- shortcuts-chest-xray/predictions/performance.py
+++ shortcuts-chest-xray/predictions/performance.py
@@ -7,29 +7,22 @@ import json
 import argparse
 from easydict import EasyDict as edict
 from sklearn import metrics
+from sklearn.metrics import average_precision_score
 
 parser = argparse.ArgumentParser(description='Check performance of the model')
 parser.add_argument('cfg_path', default=None, metavar='CFG_PATH', type=str,
                     help="Path to the config file in yaml format")
-# parser.add_argument('save_path', default=None, metavar='SAVE_PATH', type=str,
-#                     help="Path to the saved models")
-# parser.add_argument('--num_workers', default=8, type=int, help="Number of "
-#                     "workers for each data loader")
-# parser.add_argument('--device_ids', default='0,1,2,3', type=str,
-#                     help="GPU indices ""comma separated, e.g. '0,1' ")
-# parser.add_argument('--pre_train', default=None, type=str, help="If get"
-#                     "parameters from pretrained model")
-# parser.add_argument('--resume', default=0, type=int, help="If resume from "
-#                     "previous run")
-# parser.add_argument('--logtofile', default=False, type=bool, help="Save log "
-#                     "in save_path/log.txt if set True")
 parser.add_argument('--verbose', default=False, type=bool, help="Detail info")
 
 
-def performance_metrics(y_pred, y_true, sens_attr):
+def area_under_the_curve(y_pred, y_true, sens_attr):
     fpr, tpr, thresholds = metrics.roc_curve(y_true, y_pred, pos_label=1)
     auc = metrics.auc(fpr, tpr)
     return auc
+
+def average_precision(y_pred, y_true):
+    ap_score = average_precision_score(y_true, y_pred)
+    return ap_score
 
 def run(args):
     with open(args.cfg_path) as f:
@@ -37,26 +30,17 @@ def run(args):
         if args.verbose is True:
             print(json.dumps(cfg, indent=4))
 
-    # if not os.path.exists(args.save_path):
-    #     os.mkdir(args.save_path)
-    # if args.logtofile is True:
-    #     logging.basicConfig(filename=args.save_path + '/log.txt',
-    #                         filemode="w", level=logging.INFO)
-    # else:
-    #     logging.basicConfig(level=logging.INFO)
-
-    # if not args.resume:
-    #     with open(os.path.join(args.save_path, 'cfg.json'), 'w') as f:
-    #         json.dump(cfg, f, indent=1)
-
     predictions = pd.read_csv(cfg.pred_csv, index_col=0)
     y_pred = predictions['y_pred_Problem_in']
-    y_true = predictions['Pneumothorax']
-    y_pred = y_pred[~y_true.isna()]
+    y_true = predictions['Pneumothorax'].map({1.: 1 , 0.: 0, -1.: 0}).fillna(0)
+    y_pred = y_pred[y_true.isna()]
     y_true = y_true.dropna()
 
-    auc = performance_metrics(y_pred, y_true, cfg.sensitive_attribute)
+    auc = area_under_the_curve(y_pred, y_true, cfg.sensitive_attribute)
     print(auc)
+
+    ap_score = average_precision(y_pred, y_true)
+    print(ap_score)
 
 def main():
     args = parser.parse_args()
